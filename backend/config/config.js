58c25d13f45e58b4805ee1fef5a7fe5b5136c1e1
Elizabeth Awe <a.elizabeth@wallzandqueenltd.com>
2024-03-30 12:17:05 +0100

    Update

--- backend/config/config.js
+++ backend/config/config.js
@@ -17,10 +17,10 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
+    username: process.env.REMOTE_USERNAME,
+    password: process.env.REMOTE_PASSWORD,
+    database: process.env.REMOTE_DATABASE,
+    host: process.env.REMOTE_HOST,
     dialect: "mysql"
   }
 }
