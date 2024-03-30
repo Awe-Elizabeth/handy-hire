0e74623cb472faf0e183533fb9b40dd8306bc604
Elizabeth Awe <a.elizabeth@wallzandqueenltd.com>
2024-03-30 11:31:27 +0100

    Config Update

---
+++ backend/config/config.js
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv');
+dotenv.config()
+
+module.exports = {
+  development: {
+    username: process.env.REMOTE_USERNAME,
+    password: process.env.REMOTE_PASSWORD,
+    database: process.env.REMOTE_DATABASE,
+    host: process.env.REMOTE_HOST,
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  }
+}
+  
+
+  
+
